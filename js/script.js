4a7005edd902c0cae0cc357d7d3e40ab02a7c02e
AryanAD <99081149+AryanAD@users.noreply.github.com>
2023-08-26 23:01:42 +0545

    js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// autotype-top
 const typedTextSpan = document.querySelector(".typed-text");
 const cursor = document.querySelector(".cursor");
 
@@ -47,3 +48,38 @@ function erase() {
 		setTimeout(type, typingDelay + 1100);
 	}
 }
+
+// to-top-button
+let mybutton = document.getElementById("myBtn");
+
+window.onscroll = function () {
+	scrollFunction();
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
+}
+
+function topFunction() {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+}
+
+// contact-form-bottom
+const submit = document.getElementById("sub");
+const close = document.getElementById("close");
+const open = document.getElementById("open");
+const model = document.getElementById("model");
+
+open.addEventListener("click", () => model.classList.add("show-model"));
+close.addEventListener("click", () => model.classList.remove("show-model"));
+submit.addEventListener("click", () => alert("Submission Completed"));
+submit.addEventListener("click", () => model.classList.remove("show-model"));
+
+window.addEventListener("click", (e) => {
+	e.target === model ? model.classList.remove("show-model") : false;
+});
