9ea7f1f1caf873e768306183388cbf4e2a969886
AryanAD <99081149+AryanAD@users.noreply.github.com>
2023-09-02 20:05:03 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// ABOUT-autotype
+const typedTextSpan = document.querySelector(".typed-text");
+
+const words = [
+	"Web Developer",
+	"Web Designer",
+	"Student",
+	"Learner",
+	"Graphics Designer",
+];
+const typingDelay = 200;
+const erasingDelay = 200;
+const newLetterDelay = 2000;
+
+let index = 0;
+let charIndex = 0;
+
+document.addEventListener("DOMContentLoaded", () => {
+	if (words.length) {
+		setTimeout(type, newLetterDelay);
+	}
+});
+
+function type() {
+	if (charIndex < words[index].length) {
+		typedTextSpan.textContent += words[index].charAt(charIndex);
+		charIndex++;
+		setTimeout(type, typingDelay);
+	} else {
+		setTimeout(erase, newLetterDelay);
+	}
+}
+
+function erase() {
+	if (charIndex > 0) {
+		typedTextSpan.textContent = words[index].substring(0, charIndex - 1);
+		charIndex--;
+		setTimeout(erase, erasingDelay);
+	} else {
+		index++;
+		if (index >= words.length) {
+			index = 0;
+		}
+		setTimeout(type, typingDelay + 1100);
+	}
+}
